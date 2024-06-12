eb6199adc176d37cb4719e66cbf81c6996703d38
hugoresende <hugo.resende@iservices.pt>
2024-06-13 00:10:52 +0100

    access denied

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,10 +10,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/product')]
 class ProductController extends AbstractController
 {
+    #[IsGranted(new Expression('is_granted("ROLE_ADMIN")'))]
     #[Route('/', name: 'app_product_index', methods: ['GET'])]
     public function index(ProductRepository $productRepository): Response
     {
